98f34937e416c988895475700b6d906a4be63c0c
sophiarumpus <sophiarumpus@hotmail.co.uk>
2020-06-22 22:19:51 +0000

    map rendered from Google Maps API

--- assets/map.js
+++ assets/map.js
@@ -1,1 +1,7 @@
-console.log("hello");
+var map
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: { lat: 51.522573, lng: 0.467391},
+    });
+}
