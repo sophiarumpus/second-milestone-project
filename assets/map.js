53268e0217ecb7dde3d5d48ec36c082487cb0cfb
sophiarumpus <sophiarumpus@hotmail.co.uk>
2020-06-23 19:26:11 +0000

    form created

--- assets/map.js
+++ assets/map.js
@@ -1,7 +1,9 @@
 var map
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 5,
+        zoom: 4,
         center: { lat: 51.522573, lng: 0.467391},
     });
 }
+
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
