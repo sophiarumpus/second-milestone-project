6f230d84c65e3a18b26e962620c2d60a856704ab
sophiarumpus <sophiarumpus@hotmail.co.uk>
2020-06-28 21:38:24 +0000

    Markers appearing on map for Accomodation

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,16 +1,34 @@
-console.log("hi!");
-    
-    function sendMail(contactForm) {
+
+function sendMail(contactForm) {
 	emailjs.send("outlook", "project_requests", {
 		"from_name": contactForm.fullname.value,
 		"from_email": contactForm.email.value,
-		"service_request": contactForm.projectsummary.value
+		"service_request": contactForm.description.value
 		})
 		.then(
-			function(response) {
-				console.log("SUCCESS", response);
+			function(response, emailAlert) {
+                console.log("SUCCESS", response);
+                emailAlert(true);
 			},
-			function(error) {
-				console.log("FAILED", error);
-			});
+			function(error, emailAlert) {
+                console.log("FAILED", error);
+                emailAlert(false);
+            }
+        );
+        return false; // To block from loading a new page
+}
+        
+// Adapted from GitHub: Code-Institute-Solutions Code-Institute-Solutions/InteractiveFrontEndDevelopment-Resume //
+
+function emailAlert(success) {
+    if (success) {
+        Swal.fire("Message sent!")
+    }
+    else {
+        Swal.fire("oops! Something went wrong")
+    }
+}
+
+function clear() {
+    $getElementById("email-form").reset
 }
--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,60 @@
-var map
+var map;
+var service;
+var infowindow;
+
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: { lat: 57.304650, lng: -6.223610},
+  var skye = new google.maps.LatLng(57.304650, -6.223610);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+        center: skye,
+        zoom: 10
     });
+
+    // Locate restaurants nearby
+
+  var request = {
+        location: skye,
+        radius: '20000',
+        type: ['lodging']
+    };
+
+    // opens map info window
+
+    infowindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
 
-var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- 
+// Return restaurants nearby
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarkers(results[i]);
+    }
+  }
+}
+
+// Create Markers on the map for the neaby restaurants
+
+function createMarkers(place) {
+  place.geometry.location;
+  var marker = new google.maps.Marker({
+      map:map,
+      position: place.geometry.location
+  });
+
+  // listens for click on marker to display detail
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+// Accomodation Search
+
+// Restaurant Search
+
+// Attractions Search
